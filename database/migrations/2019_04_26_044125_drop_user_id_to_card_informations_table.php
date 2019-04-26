502dc7432165ace0b0c11e81f7017c903890eee3
taizou <takeda@ssjjoo.com>
2019-04-26 13:47:15 +0900

    issue717 drop user_id card_informations

--- database/migrations/2019_04_26_044125_drop_user_id_to_card_informations_table.php
+++ database/migrations/2019_04_26_044125_drop_user_id_to_card_informations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeNameUserIdToCardInformationsTable extends Migration
+class DropUserIdToCardInformationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class ChangeNameUserIdToCardInformationsTable extends Migration
     public function up()
     {
         Schema::table('card_informations', function (Blueprint $table) {
-            $table->renameColumn('user_id', 'user_code');
+            $table->dropColumn('user_id');
         });
     }
 
@@ -26,7 +26,7 @@ class ChangeNameUserIdToCardInformationsTable extends Migration
     public function down()
     {
         Schema::table('card_informations', function (Blueprint $table) {
-            $table->renameColumn('user_code', 'user_id');
+            $table->unsignedInteger('user_id')->nullable();
         });
     }
 }
