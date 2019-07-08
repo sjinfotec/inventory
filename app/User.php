3ef893e66e3178d896760f55fc1ceeb2b26d8049
taizou <takeda@ssjjoo.com>
2019-07-08 15:45:26 +0900

    issue777 UserModel追加

--- app/User.php
+++ app/User.php
@@ -40,95 +40,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    private $code;                  
-    private $department_code;                  
-    private $name;                  
-    private $kana;                  
-    private $password;                  
-    private $email;                  
-    private $employment_status;                  
-    private $working_timetable_no;                  
- 
-    public function getCodeAttribute()
-    {
-        return $this->code;
-    }
-
-    public function setCodeAttribute($value)
-    {
-        $this->code = $value;
-    }
-     
-    public function getDepartmentcodeAttribute()
-    {
-        return $this->department_code;
-    }
-
-    public function setDepartmentcodeAttribute($value)
-    {
-        $this->department_code = $value;
-    }
-
-    public function getNameAttribute()
-    {
-        return $this->name;
-    }
-
-    public function setNameAttribute($value)
-    {
-        $this->name = $value;
-    }
-     
-    public function getKanaAttribute()
-    {
-        return $this->kana;
-    }
-
-    public function setKanaAttribute($value)
-    {
-        $this->kana = $value;
-    }
-
-    public function getPasswordAttribute()
-    {
-        return $this->password;
-    }
-
-    public function setPasswordAttribute($value)
-    {
-        $this->password = $value;
-    }
-
-    public function getEmailAttribute()
-    {
-        return $this->email;
-    }
-
-    public function setEmailAttribute($value)
-    {
-        $this->email = $value;
-    }
-
-    public function getEmploymentstatusAttribute()
-    {
-        return $this->employment_status;
-    }
-
-    public function setEmploymentstatusAttribute($value)
-    {
-        $this->employment_status = $value;
-    }
-
-    public function getWorkingtimetablenoAttribute()
-    {
-        return $this->working_timetable_no;
-    }
-
-    public function setWorkingtimetablenoAttribute($value)
-    {
-        $this->working_timetable_no = $value;
-    }
-
     /**
      * カードに紐づいたユーザー取得
      *
@@ -170,28 +81,6 @@ class User extends Authenticatable
             ->get();
 
         return $data;
-    }
-
-    /**
-     * ユーザー新規登録
-     *
-     * @return void
-     */
-    public function insertNewUser(){
-        $systemdate = Carbon::now();
-        DB::table('users')->insert(
-            [
-                'code' => $this->code,
-                'employment_status' => $this->password,
-                'department_code' => $this->department_code,
-                'name' => $this->name,
-                'kana' => $this->kana,
-                'working_timetable_no' => $this->working_timetable_no,
-                'email' => $this->email,
-                'password' => $this->password,
-                'created_at'=>$systemdate
-            ]
-        );
     }
 
 }
