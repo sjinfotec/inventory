590e34c2674bba2c4642bdcbdda5aee3b2b1c64e
osamu shindo <shindo@ssjjoo.com>
2020-04-02 15:39:04 +0900

    ホームデバッグ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 
 class HomeController extends Controller
 {
@@ -26,6 +27,33 @@ class HomeController extends Controller
     {
         $authusers = Auth::user();
         $loginusercode = $authusers->code;
-        return view('home', compact('authusers'));
+        $generaluser = Config::get('const.C025.general_user');
+        $generalapproveruser = Config::get('const.C025.general_approver__user');
+        $adminuser = Config::get('const.C025.admin_user');
+        $distribution = Config::get('const.DISTRIBUTION.DISTRIBUTION');
+        $distribution43z = Config::get('const.DISTRIBUTION_VALUE.43z');
+        $distributionssjjoo = Config::get('const.DISTRIBUTION_VALUE.SSJJOO');
+        $edition = Config::get('const.EDITION.EDITION');
+        $editiondemo = Config::get('const.EDITION_VALUE.DEMO');
+        $editiontrial = Config::get('const.EDITION_VALUE.TRIAL');
+        $editioncroud = Config::get('const.EDITION_VALUE.CROUD');
+        $editionssjjoo = Config::get('const.EDITION_VALUE.SSJJOO');
+        $editionclient = Config::get('const.EDITION_VALUE.CLIENT');
+        return view('home',
+            compact(
+                'authusers',
+                'generaluser',
+                'generalapproveruser',
+                'adminuser',
+                'distribution',
+                'distribution43z',
+                'distributionssjjoo',
+                'edition',
+                'editiondemo',
+                'editiontrial',
+                'editioncroud',
+                'editionssjjoo',
+                'editionclient'
+            ));
     }
 }
