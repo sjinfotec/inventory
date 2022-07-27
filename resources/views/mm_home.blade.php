b592e7149b4be28158077456e70d5dab117830b1
akihiro itaya <itaya@ssjjoo.com>
2022-07-27 15:06:49 +0900

    fix-20220727

--- resources/views/mm_home.blade.php
+++ resources/views/mm_home.blade.php
@@ -2,11 +2,6 @@
 
 @section('content')
 <div id="home_cnt">
-					<!--
-					<div class="">
-						<h1>ホーム</h1>
-					</div>
-					-->
 					<!-- main contentns row -->
 					<div id="maincontents">
 							<mm-home
